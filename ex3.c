b32b326ec1651596ac5e98889d12bc05bc111336
tgltts <g82651504@gmail.com>
2025-08-19 21:33:18 -0300

    Create ex3.c

---
+++ ex3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  float a;
+
+  float conta;
+  
+
+  printf("digite o valor na escala celsius : ");  
+  scanf("%f", &a);
+  celsius = a;
+  contaf = (a*1.8) + 32 ;
+  
+   printf("Uma temperatura de %f graus Celsius equivale a %f graus Fahrenheit.",celsius,conta);
+
+   return 0;
+}
