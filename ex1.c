f52285f5f33bfc68f430b4cbfc5df9cb47352f5d
tgltts <g82651504@gmail.com>
2025-08-19 21:39:38 -0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int a; 
+    int b; 
+    int soma;
+    int div;
+    int mult;
+    int sub
+  
+    printf("digite o número : "); 
+    scanf("%d", &a); 
+  
+    printf("digite o número : "); 
+    scanf("%d", &b); 
+
+    soma = a + b; 
+    div = a / b;
+    mult = a * b;
+    sub = a - b;
+    
+    printf("Soma = %d\n", soma); 
+    printf("Divisão = %d\n", div);
+    printf("Multiplicação = %d\n", mult); 
+    printf("Subtração = %d\n", sub); 
+
+    return 0;
