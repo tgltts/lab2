ccb1777539efd85b7515b7813cb9d8ed1afb2196
tgltts <g82651504@gmail.com>
2025-08-19 21:25:39 -0300

    Create ex5.c

---
+++ ex5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+  float raio;
+  float area;
+  
+  const float PI = 3.1415926;
+
+  printf("Digite o valor do raio :");
+  scanf("%f", &raio);
+
+  area =  PI * (raio*raio);
+  
+
+  printf("O valor da área = %f  \n", area ,);
+
+  return 0;
+}
