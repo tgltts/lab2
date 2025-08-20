7901e7346031c1ddde81998cb52d2e3b8d0ebd64
tgltts <g82651504@gmail.com>
2025-08-19 22:35:11 -0300

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -13,11 +13,11 @@ int main()
   printf("Digite o valor do segundo cateto: ");
   scanf("%f", &cateto2);
 
-  h = sqrt ((cateto1 * cateto1) + (cateto2 * cateto2));
+  h = sqrt((cateto1*cateto1) + (cateto2*cateto2));
 
-  printf("Um triângulo retângulo com lados %f e %f tem uma hipotenusa igual a %f \n", cateto1, cateto2, h):
+  printf("Um triângulo retângulo com lados %f e %f tem uma hipotenusa igual a %f \n", cateto1, cateto2, h);
 
-  return 0:
+  return 0;
 }
   
   
