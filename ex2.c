28f9cd000c0b1bcc42d20230da6d3e6e555a0f81
tgltts <g82651504@gmail.com>
2025-08-19 21:58:24 -0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  float peso;
+  float altura;
+  float imc;
+
+  printf("Digite seu peso em quilogramas: ");
+  scanf("f%", &peso);
+
+  printf("Digite sua altura em metros: ");
+  sacanf("f%", &altura);
+
+  imc = peso / (altura * altura)
+  printf("O IMC de uma pessoa com peso %f kg e altura %f m é igual a %f \n", peso, altura, imc)
+
+
+  
